358877559fe860967c79100c0d19779374784014
Anantha Raju C <arcswdev@gmail.com>
2021-10-21 12:19:35 +0530

    ADDED RestController for for Registering Users

--- user.core/src/main/java/com/springbank/user/core/models/Account.java
+++ user.core/src/main/java/com/springbank/user/core/models/Account.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Data
@@ -13,9 +15,12 @@ import java.util.List;
 @Builder
 public class Account
 {
+    @Size(min=2, message = "username must have atleast 2 characters.")
     private String username;
 
+    @Size(min=7, message = "password must have atleast 7 characters.")
     private String password;
 
+    @NotNull(message="Specify atleast one role.")
     private List<Role> roles;
 }
